0e2994ec64a3e001df6799e25bb6ff4cc845584e
Felix Packard <felix@rigr.gg>
2024-12-27 01:15:35 +0000

    handle draws

--- packages/client/src/lib/components/GameOver.svelte
+++ packages/client/src/lib/components/GameOver.svelte
@@ -3,23 +3,28 @@
 	import { values } from "lodash";
 	import { ConfettiCannon } from "svelte-canvas-confetti";
 
-  let { client } : { client: WebSocketClient | null } = $props();
-  
-  let scores = $derived(client?.getFinalScores() ?? []);
-  let winner = $derived(scores[0].username);
+	let { client }: { client: WebSocketClient | null } = $props();
+
+	let scores = $derived(client?.getFinalScores() ?? []);
+	let winner = $derived(scores[0].username);
+	let draw = $derived(scores[0].score === scores[1].score);
 </script>
 
 <div class="flex justify-center">
-  <div class="flex p-4 flex-col gap-2 max-w-md w-full border-2 border-gray-300 rounded-lg shadow-md">
-    <h1 class="font-bold">{winner} wins!</h1>
-    <ul class="list-disc list-inside">
-      {#each scores as user}
-        <li>{user.username}: <span class="font-bold">{user.score} point{user.score === 1 ? "" : "s"}</span></li>
-      {/each}
-    </ul>
-    <!-- TODO: Change this back to <= 1 -->
-    <button disabled={!client?.isHost()} onclick={() => client?.startGame()}>Restart Game</button>
-  </div>
+	<div
+		class="flex w-full max-w-md flex-col gap-2 rounded-lg border-2 border-gray-300 p-4 shadow-md">
+		<h1 class="font-bold">{draw ? "It's a draw!" : `${winner} wins!`}</h1>
+		<ul class="list-inside list-disc">
+			{#each scores as user}
+				<li>
+					{user.username}:
+					<span class="font-bold">{user.score} point{user.score === 1 ? "" : "s"}</span>
+				</li>
+			{/each}
+		</ul>
+		<!-- TODO: Change this back to <= 1 -->
+		<button disabled={!client?.isHost()} onclick={() => client?.startGame()}>Restart Game</button>
+	</div>
 </div>
 
 <ConfettiCannon origin={[window.innerWidth / 2, window.innerHeight]} />
